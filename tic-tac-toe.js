965a64d2296fc5d80ee94f92bdfa100a20fe605e
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-10-16 12:44:20 -0500

    Added functionality to alternate plays on the board squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,6 @@
+let squareStates = [];
+let maxTurns = 9;
+let choices = ["X", "O"];
 
 window.onload = () => {
     let board = document.getElementById("board");
@@ -6,10 +9,35 @@ window.onload = () => {
     console.log(squares);
     for(let square of squares){
         setSquare(square);
+        square.onclick = click_square;
+        // square.textContent = "X";
     }
 };
 
 function setSquare(square){
     console.log(square);
     square.classList.add("square")
+}
+
+function click_square(){
+    console.log("Square clicked");
+    console.log(this);
+    if(squareStates === []){
+        this.innerText = choices[0];
+    }else if(!this.innerText){
+        squareStates.push(nextChoice());
+        this.innerText = last(squareStates);
+    }
+}
+
+function last(array){
+    return array[array.length - 1]
+}
+
+function nextChoice(){
+    if(last(squareStates) === choices[0]){
+        return choices[1];
+    }else{
+        return choices[0];
+    }
 }
